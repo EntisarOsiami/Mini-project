5774b83cb0b48d24907efe8a03479eb5cc20fd79
Entisar <Code.g.light@gmail.com>
2025-05-26 14:21:13 +0300

    ...

--- src/pages/SignupPage.jsx
+++ src/pages/SignupPage.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import SignupForm from '../components/signupForm'
+import SignupForm from '../components/SignupForm'
 function SignupPage() {
   return (
     <div><SignupForm/></div>
